71c09024713e74d0139979440e4a66ca2cfa33b8
MarceloRemeseiro <marcelo.remeseiro@gmail.com>
2024-04-25 00:52:22 +0200

    script

---
+++ streaminghub.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Nombres fijos
+PORT1=8080:8080  # Puerto de la aplicación
+PORT2=1935:1935  # Puerto RTMP
+PORT3=1936:1936  # Puerto RTSP
+PORT4=6000:6000  # Puerto SRT
+
+SCRIPT_DIR=$(dirname "$0")  # Obtiene el directorio del script
+
+# Verifica si el contenedor está corriendo y detenerlo si es necesario 1
+if [ $(docker ps -q -f name=streaminghub) ]; then
+    echo "Deteniendo y eliminando el contenedor actual..."
+    docker stop streaminghub
+    docker rm streaminghub
+else
+    echo "No existe el contenedor streaminghub, se creará uno nuevo."
+fi
+
+# Verificar si la imagen existe y eliminarla si es necesario 1
+if [ $(docker images -q myrestreamer) ]; then
+    echo "Eliminando la imagen Docker..."
+    docker rmi myrestreamer
+else
+    echo "No existe la imagen myrestreamer, se creará una nueva."
+fi
+
+# Verificar si la imagen existe y eliminarla si es necesario 2
+if [ $(docker images -q myrsui) ]; then
+    echo "Eliminando la imagen Docker..."
+    docker rmi myrsui
+else
+    echo "No existe la imagen myrsui, se creará una nueva."
+fi
+
+# Actualizar el código fuente
+echo "Actualizando el código fuente desde Git..."
+cd $SCRIPT_DIR
+git pull
+
+# Reconstruir la imagen Docker 1
+echo "Construyendo la nueva imagen Docker..."
+docker build -t myrsui .
+
+# Reconstruir la imagen Docker 2
+echo "Construyendo la nueva imagen Docker..."
+docker build --build-arg FFMPEG_IMAGE=myffmpeg --build-arg CORE_IMAGE=mycore --build-arg RESTREAMER_UI_IMAGE=myrsui -t myrestreamer .
+
+# Iniciar el nuevo contenedor
+echo "Iniciando el nuevo contenedor..."
+docker run -it --rm --name streaminghub --restart=always -p $POR1 -p $POR2 -p $POR3 -p $POR4 myrestreamer
+
+# Limpiar imágenes "dangling"
+echo "Limpiando imágenes sin usar..."
+docker image prune -f
+
+echo "Actualización completada y contenedor reiniciado."
+
+
